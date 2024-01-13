629247bf937dfe6831f12b74a7e107a324022420
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-01-14 00:01:57 +0530

    Authenticate for admin pannel

--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,10 +1,11 @@
 
 const express = require('express');
 const router = express.Router();
+const authMiddleware = require('../middleware/auth-middleware');
 
 const {adminUser} = require('../controllers/admin-controller');
 const {adminContact} = require('../controllers/admin-controller');
-router.route("/user").get(adminUser);
-router.route("/contact").get(adminContact);
+router.route("/user").get(authMiddleware,adminUser);
+router.route("/contact").get(authMiddleware,adminContact);
 
 module.exports = router;
