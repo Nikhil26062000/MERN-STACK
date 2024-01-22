b529d1e108821950c6f3cd62a6ca9214167bccbb
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-01-22 21:48:49 +0530

    fix some Convention

--- server/models/service-model.js
+++ server/models/service-model.js
@@ -8,19 +8,9 @@ const serviceSchema = new Schema({
 });
 
 
-//! Very Important note. ALWAYS GIVE THE COLLECTION NAME SAME which is in databse WHILE MAKING MODEL .
-
-//! IN THIS FILE I FIRST WROTE MY COLLECTION NAME AS "SERVICE" AND I MAKE A COLLECTION IN DATABASE AS SERVICES AS WELL, BUT STILL I WAS GETTING EMPTY RESULT BUT IN MY DATABASE IN "SERVICES" THERE WAS DATA BUT STILL IT WAS GIVING ME EMPTY ARRAY AND THE REASON IS VERY DIFFRENT..
-
-//! THE REASON BEHIND THIS IS WHEN I WRITE MY COLLECTION NAME AS "SERVICE" IN THIS FILE I THOUGHT THAT THE NAME HERE I WRITE AND THE NAME IN  MY DATABASE WILL MATCH BUT IN DATABSE IT WILL CRAEATE ANOTHER COLLECTION WHOSE NAME IS "services" and that has 0 value and thats why it is giving me empty array bcz my "SERVICE" WHICH I WROTE HERE AND "services which databse itself was created in databse" both are different .
-
-//! what my fault was that i gave my colection name as "SERVICE" and i make a collection named "SERVICES" in databse also and i was thinking ki ye dono match honge and data aajyga frontend me but jab maine yha pe "SERVICE" name dekr model create kiya tha usi time ek extra collection databse me ban gya tha jiska naam "services" tha and ye small letter me bna tha and Service and service both are different in mongo db bcz it is case sensitive. and empty isiliye aarha tha bcz Service and services me compare ho rha tha BUT mai smjh rha tha ki SERVICE AND SERVICES me hoga  
-
-//! isiliye maine yha v small letter me service likha taaki databse me khud ek services name ka collection banjaye and dono match hokr data dede frontend ko.
-
-//? ALWAYS TRY TO GIVE THE COLLECTION NAME HERE IN SMALLL LETTER AND DONT PUT  "s" in last because when i give collection name here , in databse same name of collection will be created with s in the last . 
-
-//?for eg : here i write service so in databse services will create and both will match them only i will recieve the data
-const Service = new model("service", serviceSchema);
+//if find something difficlty to d=fetch data form database then see video of mongoose in thapa technical
+// always first set thw collection name here in captial letter then in mongodb the same databse in plural form will be created then add data to that database..\
+// eg: here Service is collection name i first set here then in mongodb the same databse in plural form will be created as services and then i add data to that database
+const Service = new model("Service", serviceSchema);
 
 module.exports = Service;
